8b8a21edc471171681889d3b5d08bac899aa95c9
Dylan Allbee <dylan@allbee.org>
2016-09-14 22:52:26 -0700

    Cartesian <--> Iso conversions. Closes #2 (#3)
    * Cartesian <--> Iso conversions. Closes #2
    
    * Added documentation and changed access

---
+++ common/coordinates.h
@@ -0,0 +1,27 @@
+#pragma once
+
+typedef struct vec3_t vec3_t;
+
+struct vec3_t {
+  float x;
+  float y;
+  float z;
+};
+
+/**
+ * Convert a cartesian coordinate to isometric
+ * 
+ * @v source vector
+ * @d distance transformation vector
+ * @s scale transformation vector
+ */
+vec3_t to_iso(vec3_t v, vec3_t d, vec3_t s);
+
+/**
+ * Convert an isometric coordinate to cartesian
+ * 
+ * @v source vector
+ * @d distance transformation vector
+ * @s scale transformation vector
+ */
+vec3_t from_iso(vec3_t v, vec3_t d, vec3_t s);
