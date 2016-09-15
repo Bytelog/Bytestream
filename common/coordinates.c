31c099dedac989ff0597e54c677a04d43259e5f0
Dylan Allbee <dylan@allbee.org>
2016-09-14 22:50:42 -0700

    Added documentation and changed access

--- common/coordinates.c
+++ common/coordinates.c
@@ -1,6 +1,6 @@
 #include "coordinates.h"
 
-vec3_t scale(vec3_t v, vec3_t s) {
+static vec3_t scale(vec3_t v, vec3_t s) {
   return (vec3_t) {
     .x = v.x * s.x,
     .y = v.y * s.y,
@@ -8,7 +8,7 @@ vec3_t scale(vec3_t v, vec3_t s) {
   };
 }
 
-vec3_t translate(vec3_t v, vec3_t d) {
+static vec3_t translate(vec3_t v, vec3_t d) {
   return (vec3_t) {
     .x = v.x + d.x,
     .y = v.y + d.y,
@@ -16,7 +16,8 @@ vec3_t translate(vec3_t v, vec3_t d) {
   };
 }
 
-vec3_t map(vec3_t v) {
+// Performs cartesian -> isometric transform
+static vec3_t map(vec3_t v) {
   return (vec3_t) {
     .x = v.x + v.y,
     .y = (v.y - v.x) / 2 + v.z,
@@ -24,7 +25,8 @@ vec3_t map(vec3_t v) {
   };
 }
 
-vec3_t unmap(vec3_t v) {
+// Performs isometric -> cartesian transform
+static vec3_t unmap(vec3_t v) {
   return (vec3_t) {
     .x = v.x / 2 - v.y + v.z,
     .y = v.y + v.x / 2 - v.z,
@@ -32,10 +34,10 @@ vec3_t unmap(vec3_t v) {
   };
 }
 
-vec3_t to_iso(vec3_t v, vec3_t d, vec3_t s) {
+vec3_t coord_to_iso(vec3_t v, vec3_t d, vec3_t s) {
   return map(scale(translate(v, d), s));
 }
 
-vec3_t from_iso(vec3_t v, vec3_t d, vec3_t s) {
+vec3_t coord_from_iso(vec3_t v, vec3_t d, vec3_t s) {
   return translate(scale(unmap(v), s), d);
 }
--- common/coordinates.h
+++ common/coordinates.h
@@ -8,9 +8,20 @@ struct vec3_t {
   float z;
 };
 
-vec3_t scale(vec3_t v, vec3_t s);
-vec3_t translate(vec3_t v, vec3_t d);
-vec3_t map(vec3_t);
-vec3_t unmap(vec3_t);
+/**
+ * Convert a cartesian coordinate to isometric
+ * 
+ * @v source vector
+ * @d distance transformaton vector
+ * @s scale transformation vector
+ */
 vec3_t to_iso(vec3_t v, vec3_t d, vec3_t s);
+
+/**
+ * Convert an isometric coordinate to cartesian
+ * 
+ * @v source vector
+ * @d distance transformaton vector
+ * @s scale transformation vector
+ */
 vec3_t from_iso(vec3_t v, vec3_t d, vec3_t s);
