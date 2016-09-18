00150418a64ba60e9619a1359c2c99d992885ec6
Dylan Allbee <dylan@allbee.org>
2016-09-18 07:45:56 -0700

    Added klib dependency

--- client/client.c
+++ client/client.c
@@ -3,10 +3,17 @@
 #include <sds/sdsalloc.h>
 #include <sds/sds.h>
 #include <coordinates.h>
+#include <klib/kvec.h>
 
 int main() {
     vec3_t x = { .x = 5, .y = 6, .z = 8 };
     sds s = sdsnew("foo");
     printf("%s\n", s);
     sdsfree(s);
+
+    kvec_t(int) array;
+    kv_init(array);
+    for (int j = 0; j < 10; ++j)
+        kv_push(int, array, j);
+    kv_destroy(array);
 }
