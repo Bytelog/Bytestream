4bbc42bc32813544e9f452c59c972edaf07d6986
Dylan Allbee <dylan@allbee.org>
2016-09-18 21:04:32 -0700

    Added rand_choice/shuffle. Closes #6

--- common/rand.h
+++ common/rand.h
@@ -32,3 +32,32 @@ int64_t rand_range(rand_t *ctx, int64_t min, int64_t max);
 
 // Generate a random number from a normal distribution
 double rand_normal(rand_t *ctx);
+
+/**
+ * Shuffle an array
+ *
+ * @arr Array to shuffle
+ * @len Number of elements in the array
+ */
+#define rand_shuffle(ctx, arr, len) do {                        \
+  if ((len) > 1) {                                              \
+    size_t i, j;                                                \
+    for (i = 0; i < (len) - 1; ++i) {                           \
+        j = (size_t) rand_range((ctx), 0, (int64_t) (len));     \
+        if (i == j) continue;                                   \
+        (arr)[i] = (arr)[i] ^ (arr)[j];                         \
+        (arr)[j] = (arr)[i] ^ (arr)[j];                         \
+        (arr)[i] = (arr)[i] ^ (arr)[j];                         \
+    }                                                           \
+  }                                                             \
+} while (0)                                                     \
+
+/**
+ * Pick a random element from an array
+ *
+ * @arr  Array to choose from
+ * @len  Number of elements in the array
+ */
+#define rand_choice(ctx, arr, len) ((arr)[rand_range((ctx), 0, (len))])
+
+
