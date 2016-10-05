71a242c75b040fba0ec9bcc3124537a761ac4e72
Dylan Allbee <dylan@allbee.org>
2016-10-04 22:34:37 -0700

    Improve error handling. Closes #10

--- client/client.h
+++ client/client.h
@@ -2,29 +2,22 @@
 
 #include <stdbool.h>
 #include <stdint.h>
-#include <stdlib.h>
-#include <iniparser.h>
 #include <SDL2/SDL.h>
 
 typedef struct game_t game_t;
 typedef struct game_config_t game_config_t;
 
 struct game_t {
-  game_config_t *config;
-  SDL_Window *window;
-  SDL_Texture *buffer;
-  SDL_Renderer *renderer;
-  SDL_Rect clip;
+	game_config_t *config;
+	SDL_Window *window;
+	SDL_Texture *buffer;
+	SDL_Renderer *renderer;
+	SDL_Rect clip;
 };
 
 struct game_config_t {
-  int win_height;
-  int win_width;
-  uint32_t win_flags;
-  bool env_grid : 1;
+	int win_height;
+	int win_width;
+	uint32_t win_flags;
+	bool env_grid : 1;
 };
-
-bool game_init(game_t *);
-bool game_load_config(game_config_t *);
-void game_loop(game_t *);
-void game_exit(game_t *);
